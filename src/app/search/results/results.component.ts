08b7437730b608a252d24e4c36c23ebcd7232f35
Ross Treadway <ross@alantreadway.net>
2018-10-05 11:57:17 +0100

    Adding scss

--- src/app/search/results/results.component.ts
+++ src/app/search/results/results.component.ts
@@ -7,7 +7,7 @@ import { SearchResult, SearchResults } from '../search-result.model';
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
   @Input() public searchResults: SearchResults;
