5a92e9c64d0b5d254fc9c4195923187a67dadbee
Ross Treadway <ross@alantreadway.net>
2018-10-24 17:01:02 +0100

    Adding map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
